11d06cf0b857827ec9351ad6340c40884c40e933
dwi <dwipascanursanti020@gmail.com>
2024-04-28 14:41:39 +0700

    feat: finished mini challange 2

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
-func main () {
+func main() {
+	fmt.Println("\n", strings.Repeat("-", 20), "\n")
+	fmt.Println("Mini challange 2 - Calculate Words")
+	fmt.Println("\n", strings.Repeat("-", 20), "\n")
 
-	fmt.Println("Mini challange 1")
-	
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Please input your words: ")
+	inputString, _ := reader.ReadString('\n')
+
+	cleanInputString := strings.TrimSpace(inputString)
+
+	wordCount := make(map[string]int)
+
+	for _, char := range cleanInputString {
+		wordCount[string(char)]++
+	}
+
+	fmt.Println(wordCount)
 }
